cdbddde5c7cc450e044d2520f66fe7dda197136e
Gabriela Rojas <grojas9807@gmail.com>
2024-06-01 20:18:35 -0500

    Add styles to the form and attach images function

--- products/products.js
+++ products/products.js
@@ -6,8 +6,26 @@ const priceInput = document.getElementById('price');
 const categorySelect = document.getElementById('category');
 const messageTextarea = document.getElementById('description');
 const submitButton = form.querySelector('button[type="submit"]');
+const imagesAttached = document.getElementById("input-images")
+const preview = document.querySelector(".preview");
 
 const btnSubmit = document.getElementById('btn-add-product')
+
+//Tipos de imágenes aceptados
+const fileTypes = [
+    "image/apng",
+    "image/bmp",
+    "image/gif",
+    "image/jpeg",
+    "image/pjpeg",
+    "image/png",
+    "image/svg+xml",
+    "image/tiff",
+    "image/webp",
+    "image/x-icon",
+];
+
+// imagesAttached.style.opacity = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
     //variables 
@@ -68,6 +86,15 @@ document.addEventListener('DOMContentLoaded', function () {
         return '';
     }
 
+    function validateImages() {
+        const curFiles = imagesAttached.files;
+        if (curFiles.length === 0) {
+            imagesAttached.style.borderColor = errorColor;
+            return 'Por favor añada imágenes de su producto.';
+        }
+        return '';
+    }
+
     function showError(message) {
         errorParagraph.innerHTML = message;
     }
@@ -101,10 +128,18 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
+        errorMessage = validateImages();
+        if (errorMessage) {
+            showError(errorMessage);
+            return;
+        }
+
         errorParagraph.innerHTML = '';
         agregarProducto();
         // form.submit(); --> Envía el form y recarga la página     
     });
+
+    imagesAttached.addEventListener("change", updateImageDisplay);
 
     submitButton.addEventListener('click', function(event) {
         
@@ -119,70 +154,80 @@ const productos = [
         nombre: "Laptop Dell Inspiron",
         precio: 1000,
         descripcion: "Laptop Dell Inspiron con 16GB RAM y 512GB SSD",
-        categoria: "Laptops"
+        categoria: "Laptops",
+        imagen: "https://exitocol.vteximg.com.br/arquivos/ids/22241706/Celular-XIAOMI-Redmi-Note-13-PRO-256-GB-8-GB-RAM-LILA-3497841_a.jpg"
     },
     {
         id: 2,
         nombre: "Smartphone Samsung Galaxy S21",
         precio: 800,
         descripcion: "Smartphone Samsung Galaxy S21 con 128GB de almacenamiento",
-        categoria: "Smartphones"
+        categoria: "Smartphones",
+        imagen: "https://exitocol.vteximg.com.br/arquivos/ids/22241706/Celular-XIAOMI-Redmi-Note-13-PRO-256-GB-8-GB-RAM-LILA-3497841_a.jpg"
     },
     {
         id: 3,
         nombre: "Tablet Apple iPad Pro",
         precio: 1200,
         descripcion: "Apple iPad Pro de 12.9 pulgadas, 256GB",
-        categoria: "Tablets"
+        categoria: "Tablets",
+        imagen: "https://exitocol.vteximg.com.br/arquivos/ids/22241706/Celular-XIAOMI-Redmi-Note-13-PRO-256-GB-8-GB-RAM-LILA-3497841_a.jpg"
     },
     {
         id: 4,
         nombre: "Monitor LG UltraWide",
         precio: 300,
         descripcion: "Monitor LG UltraWide de 34 pulgadas, 2560x1080",
-        categoria: "Monitores"
+        categoria: "Monitores",
+        imagen: "https://exitocol.vteximg.com.br/arquivos/ids/22241706/Celular-XIAOMI-Redmi-Note-13-PRO-256-GB-8-GB-RAM-LILA-3497841_a.jpg"
     },
     {
         id: 5,
         nombre: "Auriculares Sony WH-1000XM4",
         precio: 350,
         descripcion: "Auriculares inalámbricos con cancelación de ruido",
-        categoria: "Auriculares"
+        categoria: "Auriculares",
+        imagen: "https://exitocol.vteximg.com.br/arquivos/ids/22241706/Celular-XIAOMI-Redmi-Note-13-PRO-256-GB-8-GB-RAM-LILA-3497841_a.jpg"
     },
     {
         id: 6,
         nombre: "Reloj Inteligente Apple Watch Series 6",
         precio: 500,
         descripcion: "Apple Watch Series 6 con GPS, 44mm",
-        categoria: "Relojes Inteligentes"
+        categoria: "Relojes Inteligentes",
+        imagen: "https://exitocol.vteximg.com.br/arquivos/ids/22241706/Celular-XIAOMI-Redmi-Note-13-PRO-256-GB-8-GB-RAM-LILA-3497841_a.jpg"
     },
     {
         id: 7,
         nombre: "Cámara Canon EOS R",
         precio: 1800,
         descripcion: "Cámara sin espejo de fotograma completo, 30.3MP",
-        categoria: "Cámaras"
+        categoria: "Cámaras",
+        imagen: "https://exitocol.vteximg.com.br/arquivos/ids/22241706/Celular-XIAOMI-Redmi-Note-13-PRO-256-GB-8-GB-RAM-LILA-3497841_a.jpg"
     },
     {
         id: 8,
         nombre: "Impresora HP LaserJet Pro",
         precio: 250,
         descripcion: "Impresora láser monocromática inalámbrica",
-        categoria: "Impresoras"
+        categoria: "Impresoras",
+        imagen: "https://exitocol.vteximg.com.br/arquivos/ids/22241706/Celular-XIAOMI-Redmi-Note-13-PRO-256-GB-8-GB-RAM-LILA-3497841_a.jpg"
     },
     {
         id: 9,
         nombre: "Router Asus RT-AC86U",
         precio: 200,
         descripcion: "Router Wi-Fi de doble banda con tecnología MU-MIMO",
-        categoria: "Routers"
+        categoria: "Routers",
+        imagen: "https://exitocol.vteximg.com.br/arquivos/ids/22241706/Celular-XIAOMI-Redmi-Note-13-PRO-256-GB-8-GB-RAM-LILA-3497841_a.jpg"
     },
     {
         id: 10,
         nombre: "Teclado Mecánico Corsair K95",
         precio: 200,
         descripcion: "Teclado mecánico RGB con interruptores Cherry MX Speed",
-        categoria: "Teclados"
+        categoria: "Teclados",
+        imagen: "https://exitocol.vteximg.com.br/arquivos/ids/22241706/Celular-XIAOMI-Redmi-Note-13-PRO-256-GB-8-GB-RAM-LILA-3497841_a.jpg"
     }
 ];
 
@@ -199,7 +244,8 @@ function agregarProducto() {
         nombre: nameValue,
         precio: priceValue,
         descripcion: descriptionValue,
-        categoria: categoryValue
+        categoria: categoryValue,
+        imagen: imagesAttached
     };
 
     productos.push(nuevoProducto);
@@ -211,6 +257,51 @@ function agregarProducto() {
 
     form.reset();
 }
+
+function updateImageDisplay(){
+    const curFiles = imagesAttached.files;
+    if (curFiles.length > 0) {
+        const list = document.createElement("ol");
+        preview.appendChild(list)
+        preview.classList.add('active');
+    
+        for (const file of curFiles) {
+            const listItem = document.createElement("li");
+            const para = document.createElement("p");
+            if (validFileType(file)) {
+                para.textContent = `Nombre de archivo: ${file.name}, peso: ${returnFileSize(file.size,)}.`;
+                const image = document.createElement("img");
+                image.src = URL.createObjectURL(file);
+                image.alt = image.title = file.name;
+                image.className = "img-attached"
+        
+                listItem.appendChild(image);
+                listItem.appendChild(para);
+            } else {
+                para.textContent = `El archivo ${file.name}: no tiene una extensión válida. Seleccione otra imagen.`;
+                listItem.appendChild(para);
+            }
+        
+            list.appendChild(listItem);
+        }
+    }
+}
+
+function validFileType(file) {
+    return fileTypes.includes(file.type);
+}
+
+function returnFileSize(number) {
+    if (number < 1024) {
+        return `${number} bytes`;
+    } else if (number >= 1024 && number < 1048576) {
+        return `${(number / 1024).toFixed(1)} KB`;
+    } else if (number >= 1048576) {
+        return `${(number / 1048576).toFixed(1)} MB`;
+    }
+}
+
+
 
 //Cargar imágenes del producto 
 // const input = document.querySelector(".input-images");
